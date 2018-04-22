ed80c95ebe38d91d2a5ff6dd43c5cddcd7315623
mwie <mwiegand@seibert-media.net>
2018-04-22 12:12:05 +0200

    Hash.each_key and Hash.each_value compatibility

--- quickeach.rb
+++ quickeach.rb
@@ -4,7 +4,6 @@ class QuickEach
   def initialize enumerable
     @enumerable = enumerable
   end
-
   def method_missing method, *args, &block
     @enumerable.collect do |element|
       element.send method, *args, &block
@@ -15,16 +14,48 @@ end
 class Array
   # copy method
   define_method(
-    "__each".to_sym,
-    [].method(:each)
+    "_original_each".to_sym,
+    instance_method(:each)
   )
 
   def each &block
-    return QuickEach.new self unless block_given?
-    __each &block 
+    unless block_given?
+      return QuickEach.new self
+    else
+      _original_each &block 
+    end
   end
 end
 
-# array = ["alice", "bob", "jon doe"]
-# array.each.length 
-# => [5, 3, 6]
+class Hash
+  # copy method
+  define_method(
+    "_original_each_key".to_sym,
+    instance_method(:each_key)
+  )
+  def each_key &block
+    unless block_given?
+      return QuickEach.new self.keys
+    else
+      _original_each_key &block
+    end
+  end
+  define_method(
+    "_original_each_value".to_sym,
+    instance_method(:each_value)
+  )
+  def each_value &block
+    unless block_given?
+      return QuickEach.new self.values
+    else
+      _original_each_value &block
+    end
+  end
+end
+
+# test
+#a = ["alice", "bob", "jon doe"]
+#p a.each.length 
+#h = {alice: "wonderland", bob: "dylan", jon: "doe"}
+#p h.each_key.length
+#p h.each_value.length
